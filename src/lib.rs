f8f1d52513f0b81bcb72f35e238b161edfca5034
root <root@EPI-DEV-W10-NMT.gtt.fr>
2024-12-10 17:12:37 +0100

    feat: use result in build method

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,25 @@
+use std::fmt;
+
 pub struct ThreadPool {
     size: usize,
 }
 
+#[derive(Debug)]
+pub struct PoolCreationError;
+
+impl fmt::Display for PoolCreationError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", "Error related to ThreadPool")
+    }
+}
+
 impl ThreadPool {
-    pub fn new(size: usize) -> Self {
-        return Self { size };
+    pub fn build(size: usize) -> Result<ThreadPool, PoolCreationError> {
+        if size > 0 {
+            Ok(Self { size })
+        } else {
+            Err(PoolCreationError)
+        }
     }
 
     pub fn execute<F>(&self, f: F)
