9e1f7bed03fa595f7b4818a7aa3772395fdaf7f0
root <root@EPI-DEV-W10-NMT.gtt.fr>
2024-12-15 15:10:50 +0100

    implement worker closure

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,19 @@
 use std::fmt;
+use std::sync::mpsc::{self, Receiver};
+use std::sync::{Arc, Mutex};
 use std::thread;
 
 pub struct ThreadPool {
     threads: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
 
 struct Worker {
     id: usize,
     thread: thread::JoinHandle<()>,
 }
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
 #[derive(Debug)]
 pub struct PoolCreationError;
@@ -20,8 +25,14 @@ impl fmt::Display for PoolCreationError {
 }
 
 impl Worker {
-    fn new(id: usize) -> Self {
-        let thread = thread::spawn(|| {});
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Self {
+        let thread = thread::spawn(move || loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+
+            println!("Worker {id} got a job; executing.");
+
+            job();
+        });
         Worker { id, thread }
     }
 }
@@ -29,13 +40,17 @@ impl Worker {
 impl ThreadPool {
     pub fn build(size: usize) -> Result<ThreadPool, PoolCreationError> {
         if size > 0 {
+            let (sender, receiver) = mpsc::channel();
+
+            let receiver = Arc::new(Mutex::new(receiver));
+
             let mut threads = Vec::with_capacity(size);
 
             for i in 0..size {
-                threads.push(Worker::new(i));
+                threads.push(Worker::new(i, Arc::clone(&receiver)));
             }
 
-            Ok(Self { threads })
+            Ok(Self { threads, sender })
         } else {
             Err(PoolCreationError)
         }
@@ -45,5 +60,7 @@ impl ThreadPool {
     where
         F: FnOnce() + Send + 'static,
     {
+        let job = Box::new(f);
+        self.sender.send(job).unwrap();
     }
 }
