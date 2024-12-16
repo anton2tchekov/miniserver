c6933db5cd42012e79b1554957bcd401bda470ee
root <root@EPI-DEV-W10-NMT.gtt.fr>
2024-12-16 11:56:18 +0100

    fix assets path for sleep route

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,7 @@ fn handle_connection(mut stream: TcpStream) {
         "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "assets/hello.html"),
         "GET /sleep HTTP/1.1" => {
             thread::sleep(Duration::from_secs(5));
-            ("HTTP/1.1 200 OK", "hello.html")
+            ("HTTP/1.1 200 OK", "assets/hello.html")
         }
         _ => ("HTTP/1.1 404 NOT FOUND", "assets/404.html"),
     };
